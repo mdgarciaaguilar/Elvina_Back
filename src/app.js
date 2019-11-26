672486e0c3ca9a7620a2b7e584a217e36a8f37bb
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2019-11-26 13:36:07 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
+var cors = require('cors');
 
 require('./db/db.js')
 
 const router = require('./routes.js')
 
-
+const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+app.use(cors())
+app.use(express.json()) // parsea a json
 app.use(router)
+
 
 app.listen(port, function() {
   console.log('Server up and running on port', port)
